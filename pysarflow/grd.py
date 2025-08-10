c62738e030c37a7eafc68122995ef358bf0bfd43
Ethel Ogallo <ogalloethel@gmail.com>
2025-08-10 15:47:40 +0200

    add read SAFE product function

--- pysarflow/grd.py
+++ pysarflow/grd.py
@@ -18,34 +18,38 @@ This file can also be imported as a module and contains the following functions:
         *
 """
 
-from esa_snappy import ProductIO
 import os
+import esa_snappy
+import numpy as np
+import matplotlib.pyplot as plt
+
+from esa_snappy import Product, ProductIO, ProductUtils, WKTReader, HashMap, GPF, jpy
 
 
-def read_grd_product(product_path):
+# Loads the SNAP operators globally when the module is imported
+GPF.getDefaultInstance().getOperatorSpiRegistry().loadOperatorSpis()
+
+def read_SAFE_product(file_path):
     """
-    Reads a Sentinel-1 GRD product using SNAP's ProductIO.
+    Reads a Sentinel-1 SAR GRD product from a .SAFE directory or a .zip archive.
 
-    This function checks whether the provided product path exists on disk, then
-    attempts to load the product using ProductIO.readProduct. If the product
-    cannot be read, it raises a RuntimeError with details about the failure.
+    This function utilizes ESA SNAP's `ProductIO.readProduct` to load the SAR data
+    into a SNAP Product object, which is the base object for further processing.
 
     Args:
-        product_path (str): Path to the Sentinel-1 SAFE format product directory or file.
+        file_path (str): The file path to the Sentinel-1 .SAFE directory (unzipped)
+                        or the .zip archive containing the GRD product.
 
     Returns:
-        product: The SNAP product object representing the loaded Sentinel-1 data.
+        esa_snappy.Product: A SNAP Product object representing the loaded SAR data.
 
     Raises:
-        FileNotFoundError: If the specified product path does not exist.
-        RuntimeError: If reading the product fails due to unexpected errors.
+        FileNotFoundError: If the specified zip_path does not exist.
     """
-    try:
-        if not os.path.exists(product_path):
-            raise FileNotFoundError(f"Product path does not exist: {product_path}")
-        product = ProductIO.readProduct(product_path)
-        return product
-    except Exception as e:
-        raise RuntimeError(
-            f"An error occurred while reading the grd product: {str(e)}"
-        ) from e
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"Product path not found: {file_path}")
+    
+    print(f"Reading SAR product from: {file_path}...")
+    product = ProductIO.readProduct(file_path)
+    print("\tProduct read successfully.")
+    return product
